af037587962f2e1a10fc51f07a1977812c3d0483
Alex <zaychenko96y@outlook.com>
2020-04-25 17:47:07 +0300

    Review commit

--- src/adapters/console.adapter.js
+++ src/adapters/console.adapter.js
@@ -1,28 +1,31 @@
 const readline = require('readline');
-const { stdin, exit } = process;
+const { exit } = process;
 
 const TERMINATION_CODE = 'SIGINT';
+const COLOR_CODE = '\x1b[32m'
+const RESET_COLOR_CODE = '\x1b[0m'
 let io = null;
 
-const renderOptionsChoice = (options, currentChoice = 0) => {
-  const { output } = io;
-  readline.moveCursor(output, -2, -options.length - 1); // -2 and -1 are smell code to counter main loop
-  options.forEach((option, i) => {
-    const filler = i === currentChoice ? '-' : ' ';
-    io.write(`${filler}${option}\n`);
-  });
+const renderOptions = (options, index) => {
+  options
+    .map((option, i) => i === index ? `${COLOR_CODE}${option}${RESET_COLOR_CODE}` : option)
+    .forEach(option => io.write(`${option}\n`))
 }
 
-const handleKeystrokes = (options, currentChoice = 0) => {
-  const { input } = io;
-  readline.emitKeypressEvents(input);
-  stdin.setRawMode(true);
-  stdin.on('keypress', (_, { name }) => {
-    if (name === 'down'){
-      currentChoice++;
-      if (currentChoice > options.length - 1)
-        currentChoice = 0;
-      renderOptionsChoice(options, currentChoice);
+const handleOptionOnKeyPress = options => {
+  let currentIndex = 0
+  io.input.on('keypress', (_, { name }) => {
+    if (name === 'down') {
+      currentIndex++
+      if (currentIndex > options.length - 1)
+        currentIndex = 0
+      renderOptions(options, currentIndex)
+    }
+    if (name === 'up') {
+      currentIndex--
+      if (currentIndex < 0)
+        currentIndex = options.length - 1
+      renderOptions(options, currentIndex)
     }
   });
 }
@@ -35,9 +38,8 @@ module.exports = Object.freeze({
   getCurrentInput: prefix => new Promise(resolve => io.question(prefix, answer => resolve(answer))),
   exitOnTermination: () => io.on(TERMINATION_CODE, () => exit(0)),
   handleOptionsList: options => {
-    io.write('Please, choose one of the following options:\n\n\n'); // multiple \n are smell code to counter main loop
-    options.forEach(option => io.write(` ${option}\n`));
-    renderOptionsChoice(options);
-    handleKeystrokes(options);
+    io.write('Please, choose one of the following options:\n');
+    renderOptions(options, 0)
+    handleOptionOnKeyPress(options)
   }
 })
